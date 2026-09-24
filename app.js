e03a420e1175872e1b8c3689256abd5331ffeda8
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-24 14:30:39 -0300

    refactor(app):  agregar y utilizar express en el archivo app.js ademas eliminar acceso a la base de datos para su siguiente configuracion

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import ordersRouter from './routes/orders.js'
-import usersRouter from './routes/users.js'
+import express from 'express';
 
+import ordersRouter from './routes/orders.js';
+import usersRouter from './routes/users.js';
 
-const app = express()
-app.use(express.json())
-
-mongoose.connect('mongodb://localhost:27017/shipnow')
-  .then(() => console.log('Mongo conectado'))
+const app = express;
 
 app.use('/api/orders', ordersRouter)
 app.use('/api/users', usersRouter)
 
 app.listen(PORT, () => console.log('ok'))
+
+export default app;
