059365d8bd9d4aa0de79fc62014a8c2eb1aea6e3
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-24 14:42:42 -0300

    refctor(app); eliminar la escucha al PORT y configurar la app con express

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ import express from 'express';
 import ordersRouter from './routes/orders.js';
 import usersRouter from './routes/users.js';
 
-const app = express;
+const app = express();
+
+app.use(express.json())
 
 app.use('/api/orders', ordersRouter)
 app.use('/api/users', usersRouter)
 
-app.listen(PORT, () => console.log('ok'))
 
 export default app;
