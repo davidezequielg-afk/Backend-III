48ae06080484d7f295c8cf23ed32974835c0af03
davidezequielg-afk <davidezequielg@icloud.com>
2026-09-24 21:34:28 -0300

    refactor(config): agregar configuracion del .env para su uso de datos

---
+++ config/config.env.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const MONGO_URL = process.env.MONGODB_URI
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const { NODE_ENV } = process.env;
